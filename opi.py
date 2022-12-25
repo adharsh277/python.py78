f5b2d537132fbd36adce626232fc035875d3bff6
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2022-12-25 23:11:32 +0530

    Create opi.py

---
+++ opi.py
@@ -0,0 +1,19 @@
+# ADD ITMES'
+thisdict = {
+    'brand': 'ford',
+    'model': 'mustang',
+    'year': '1897'
+}
+thisdict.update({'color': 'red'})
+print(thisdict)
+# rEMove items
+thisdict = {
+    'brand': 'marutyhi',
+    'model': 'mi8989',
+    'year': '1898'
+}
+print(thisdict)
+thisdict.pop('model')
+print(thisdict)
+thisdict.clear()
+print(thisdict)
