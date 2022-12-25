e3ef86a0affc0d2e0b57106e6d396c06812fcbad
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2022-12-25 23:08:19 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,85 @@
+# arthemtic operators
+# addition operators
+print(980+20)
+print(90+20)
+print(80+20)
+print(98+20)
+print(9850+20)
+print(956+20)
+# subtraction operators
+print(12-11)
+print(73-13)
+print(45-16)
+print(52-18)
+print(55-23)
+# multiplication operators
+print(107*98)
+print(14*98)
+print(140*98)
+print(11*98)
+print(10*98)
+# Exponent operators
+print(2**1)
+print(5**12)
+print(1**17)
+print(3**1)
+print(4**6)
+# modlus operator
+print(2 % 50)
+print(10 % 60)
+print(30 % 50)
+print(60 % 90)
+print(15 % 40)
+# floor division
+print(6//2)
+print(9//4)
+print(98//2)
+print(74//5)
+print(34//8)
+# assignment operator
+
+a = 5  # assiging the value 5 to varibale a
+print(a)
+a += 3  # it means same as a=a+3
+print(a)
+a -= 2  # it means same as a=a-2
+print(a)
+a *= 5  # it means same as a=a*5
+print(a)
+a /= 6  # it means same as a=a/5
+print(a)
+a %= 3  # it means same as a=a%3
+print(a)
+a **= 3  # it means same as a=a**3
+print(a)
+a //= 4  # it means same as a=a//4
+print(a)
+
+# 3..Comparision operators
+
+print(3 == 4)  # equality check
+
+print(3 != 4)  # not equality check
+
+print(3 > 4)  # greater then check
+
+print(3 < 4)  # less then check
+
+print(5 >= 6)  # greater then equality check
+
+print(5 <= 6)  # less then equality check\
+
+# 4..Logical operators
+x = 2
+print(x < 5 and x < 8)  # return true only if both the statements are true
+print(x > 3 and x > 1)
+
+print(x > 2 or x > 1)  # return true if atleast one of the conditions are true
+print(x > 2 or x > 3)
+
+print(not (x > 2 or x > 3))  # it returns the reversed result(vice versa)
+
+# 5..identity operators
+a = [10, 20]
+b = [10, 20]
+c = a
