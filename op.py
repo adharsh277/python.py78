922f0ddf8f8d0b25237c1cea2cf03a3fbe88e84e
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-01-01 23:55:04 +0530

    Create op.py

---
+++ op.py
@@ -0,0 +1,31 @@
+# ydff
+x = 10
+y = "jojo rodriguez"
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+# 6556
+x = str(10)
+y = int(10)
+z = float(10.8)
+print(x, y, z)
+print(z+y)
+
+x = ("my name is johon")
+print(x)
+# slicing
+x = 'with great power comes great responsiblity , so peter its time for you to choose your way'
+y = 'aunt may i will be the same dont worry '
+print(x[2:5])
+print(y[2:6])
+print(y[2:-9])
+print(y[0:])
+print(x.upper())
+print(y.upper())
+print(x.lower())
+print(x.lower())
+print(x.split())
+print(y.split())
+print(x.replace('great', 'better'))
+print(x.replace('t', 'be'))
