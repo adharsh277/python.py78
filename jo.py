9cc584548ffb56060d31e2a670d9b79209de2e64
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-01-01 23:54:17 +0530

    Create jo.py

---
+++ jo.py
@@ -0,0 +1,44 @@
+
+# default parameter value
+def myFunc(age='40'):
+    print('My age is ' + age)
+
+
+myFunc('10')
+myFunc('20')
+myFunc()
+
+# default parameter value
+# def myFunc(age = '40'):
+#     print('My age is ' + age)
+
+# myFunc('10')
+# myFunc('20')
+# myFunc()
+
+# Funstions either do something or returns something
+
+
+# def printFruit(fruits):
+#     print(fruits)
+#     for fruit in fruits:
+#         print(fruit)
+
+# fruits = ['apples', 'bananas', 'oranges']
+# printFruit(fruits)
+
+
+# def printAge():
+#     print(10)
+#     print(10)
+#     return 20
+
+
+# print(printAge())
+
+def myFunc(x):
+    return 2*x
+
+
+print(myFunc(5))
+print(myFunc(6))
