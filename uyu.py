9d0d17e9a0cec0c676b942d5f3f2061398f63bb4
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-01-12 18:29:05 +0530

    Create uyu.py

---
+++ uyu.py
@@ -0,0 +1,7 @@
+tuple1 = ("abc", 34, True, 40, "male")
+
+print(tuple1)
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
